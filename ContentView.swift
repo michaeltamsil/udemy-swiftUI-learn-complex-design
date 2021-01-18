3645e54a97122c01d967c6557bee4a993a8695bb
Michael Tamsil <michaeltamsil@gmail.com>
2021-01-18 15:00:46 +0700

    set content view like card

--- ContentView.swift
+++ ContentView.swift
@@ -9,8 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(UIColor(red: 0.09, green: 0.63, blue: 0.52, alpha: 1.00))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("elon-musk")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                
+                Text("Angela Yu")
+                    .fontWeight(.bold)
+                    .padding()
+                    .font(Font.custom("HachiMaruPop-Regular", size: 40))
+                    .foregroundColor(.white)
+                
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                
+                Divider()
+                
+                InfoView(imageName: "phone.fill", text: "062 0878 9425 5212")
+                InfoView(imageName: "envelope.fill", text: "micheltamsil@gmail.com")
+                
+                
+            }
+                
+        }
     }
 }
 
@@ -19,3 +50,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
